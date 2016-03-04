d4612d0ee7edd825c9363f027d55abd58de7f0e0
Brian Kemper <bkemper@gmail.com>
2016-03-03 17:37:10 -0700

    on step 7 now

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -1,4 +1,105 @@
+var me = {
+    name: "Brian Kemper"
+  , location: "Midvale, UT"
+  , occupations: ["Programmer", "Eater", "Runner", "ZZZZZer"]
+  , hobbies: [
+    {name: "Running"
+  , type: "Current"}
+  ,{
+    name: "Fishing"
+  , type: "Past"
+  }
+  ]
+  , skillz: [
+    {
+      "id": 1,
+      "name": "Javascript",
+      "experience": "Intermediate"
+    }
+    , {
+      "id": 2,
+      "name": "Person",
+      "experience": "Intermediate"
+    }
+    , {
+      "id": 3,
+      "name": "Sitting",
+      "experience": "Master"
+    }
+  ]
+};
+
 module.exports = {
+
+  getName: function(req, res, next) {
+    res.status(200).json({name: me.name});
+},
+  getLocation: function(req, res, next) {
+    res.status(200).json({location: me.location});
+},
+//localhost:8080/occupations/?order=asc
+  getOccupations: function(req, res, next) {
+    var order = req.query.order;
+    if (order === 'asc') {
+      res.status(200).json({occupations: me.occupations.sort()});
+    } else if  (order === 'desc') {
+      res.status(200).json({occupations: me.occupations.sort().reverse()});
+    } else {
+      res.status(200).json({occupations: me.occupations});
+    }
+},
+  getOccupationsLatest: function(req, res, next) {
+    res.status(200).json({occupations: me.occupations[me.occupations.length-1]});
+},
+  getHobbies: function(req, res, next) {
+    res.status(200).json({hobbies: me.hobbies});
+},
+  getSkillz: function(req, res, next) {
+    var exp = req.query.experience;
+    var ret = [];
+    if (exp) {
+      me.skillz.forEach(function(obj){
+        if(obj.experience === exp) {
+          ret.push(obj);
+        }
+      });
+      res.status(200).json(ret);
+    } else {
+      res.status(200).json(me.skillz);
+    }
+},
+  getHobbiesType: function(req, res, next) {
+    var type = req.params.type;
+    var ret = [];
+    me.hobbies.forEach(function(obj){
+      if(obj.type === type) {
+        ret.push(obj.name);
+      }
+    });
+    res.status(200).json(ret);
+},
+  putName: function(req, res, next) {
+    me.name = req.body.name;
+    res.status(200).json(me.name);
+},
+  putLocation: function(req, res, next) {
+    me.location = req.body.location;
+    res.status(200).json(me.location);
+},
+  postHobbies: function(req, res, next) {
+    me.hobbies.push(req.body);
+    res.status(200).json(me.hobbies);
+},
+  postOccupations: function(req, res, next) {
+    me.occupations.push(req.body.occupations);
+    res.status(200).json(me.occupations);
+},
+  postSkillz: function(req, res, next) {
+    var skillz = req.body.skillz;
+    skillz.id = me.skillz.length + 1;
+    me.skillz.push(skillz);
+    res.status(200).json(me.skillz[me.skillz.length - 1]);
+  }
 
 
 
--- index.js
+++ index.js
@@ -8,6 +8,21 @@ var express = require('express')
 app.use(bodyParser.json());
 app.use(middleware.addHeaders);
 
-app.listen(post, function(){
+app.get('/name', mainCtrl.getName);
+app.get('/location', mainCtrl.getLocation);
+app.get('/occupations', mainCtrl.getOccupations);
+app.get('/occupations/latest', mainCtrl.getOccupationsLatest);
+app.get('/hobbies', mainCtrl.getHobbies);
+app.get('/hobbies/:type', mainCtrl.getHobbiesType);
+app.get('/skillz', mainCtrl.getSkillz);
+
+app.put('/name', mainCtrl.putName);
+app.put('/location', mainCtrl.putLocation);
+
+app.post('/hobbies', mainCtrl.postHobbies);
+app.post('/occupations', mainCtrl.postOccupations);
+app.post('/skillz', mainCtrl.postSkillz);
+
+app.listen(port, function(){
   console.log('Listening on port: ' + port);
 });
